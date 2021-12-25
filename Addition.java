a1df3eb00b5d682929b353431600b5e08aa0a882
niludarekar1 <nilu.darekar1@gmail.com>
2021-12-25 19:05:58 +0530

    Added files

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.lang.*;
+import com.company.Addition;
+
+public class Addition {
+    public static void main(String args[]){
+
+        int j,n;
+        int i = 10;
+        j=20;
+        n=i+j;
+        System.out.println("Addition of two no "+ n);
+
+    }
+}
